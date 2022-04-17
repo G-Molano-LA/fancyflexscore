649a7f773a7ba6b0ad3c97b6fd9e136d0af53c8b
G-Molano-LA <52918763+G-Molano-LA@users.noreply.github.com>
2022-04-18 00:16:52 +0200

    NCBI programs at the begging on the $PATH

--- requirements.py
+++ requirements.py
@@ -3,7 +3,7 @@ import subprocess
 # Blastp and psiblast: https://www.ncbi.nlm.nih.gov/books/NBK569861/
 subprocess.run(["wget", "https://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz"], check=True)
 subprocess.run(["tar", "zxvpf", "ncbi-blast-2.13.0+-x64-linux.tar.gz"], check=True)
-subprocess.run(["export", "PATH=$PATH:$HOME/ncbi-blast-2.13.0+/bin"], check=True) # temporary solution. profile must be modified
+subprocess.run(["export", "PATH=$PWD/ncbi-blast-2.13.0+/bin:$PATH"], check=True) # temporary solution. profile must be modified
 
 # Tcoffe: https://www.tcoffee.org/Projects/tcoffee/workshops/tcoffeetutorials/installation.html
 subprocess.run(["sudo", "apt-get", "install", "t-coffee"], check=True) # sudo apt-get install t-coffee
